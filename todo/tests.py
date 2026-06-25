1d610b4ede8c9a51b9121b5196e304ba3d1095b5
1F10250112 <s1f102501125@iniad.org>
2026-06-25 11:14:17 +0900

    Fixed Flake8 warning #2

--- todo/tests.py
+++ todo/tests.py
@@ -6,12 +6,10 @@ from todo.models import Task
 # Create your tests here.
 class SampleTestCase(TestCase):
 
-    
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
 
 class TaskModelTestCase(TestCase):
-
 
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
@@ -41,7 +39,6 @@ class TaskModelTestCase(TestCase):
         self.assertFalse(task.is_overdue(current))
 
 class TodoViewTestCase(TestCase):
-
 
     def test_index_get(self):
         client = Client()
